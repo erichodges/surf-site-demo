e039c9892bd804ed5aec083eb7c5be275b451f01
Eric Hodges <ehodges2004@gmail.com>
2018-08-02 12:56:34 -0700

    move nav and footer to components

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer :fixed="fixed" app class="black">
+    <span>&copy; 2018</span>
+  </v-footer>
+</template>
--- layouts/index.vue
+++ layouts/index.vue
@@ -1,37 +1,26 @@
 <template>
   <v-app light>
     <div class="div-with-background">
-      <v-toolbar dense flat color="transparent">
-        <v-toolbar-title>
-          <v-btn to="/" color="transparent" flat>
-          <h2 class="black--text">Site Logo</h2></v-btn>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn to="/signin" flat color="blue"><h3>Sign In</h3></v-btn>
-          <v-btn to="/signup" flat color="blue"><h3>Sign Up</h3></v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-
-      
-        
-          <nuxt />
-        
-      
-
-      <v-footer :fixed="fixed" app class="white">
-        <span>&copy; 2018</span>
-      </v-footer>
+      <Nav />
+      <nuxt />
+      <Footer />
     </div>
   </v-app>
 </template>
 
 <script>
+  import Nav from '~/components/Nav'
+  import Footer from '~/components/Footer'
+
   export default {
+    components: {
+      Nav,
+      Footer
+    },
     data () {
       return {
         clipped: false,
-        drawer: false,
+        drawer: true,
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
@@ -58,7 +47,7 @@
 }
 
 /* this removes all caps from the buttons */
-  #app .btn {
+  #app .v-btn {
     text-transform: none;
   }
 
