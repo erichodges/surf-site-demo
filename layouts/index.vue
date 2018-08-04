fceabd280fb996d1f0ced8c3f42dcf3a93393792
Eric Hodges <ehodges2004@gmail.com>
2018-08-04 15:30:00 -0700

    added nav buttons

--- layouts/index.vue
+++ layouts/index.vue
@@ -35,8 +35,9 @@
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn to="/signin" flat color="blue"><h3>Sign In</h3></v-btn>
-          <v-btn to="/signup" flat color="blue"><h3>Sign Up</h3></v-btn>
+          <v-btn to="/signin" flat color="white"><h3>Products</h3></v-btn>
+          <v-btn to="/signup" flat color="white"><h3>About</h3></v-btn>
+          <v-btn to="/#" flat color="white"><h3>Contact</h3></v-btn>
         </v-toolbar-items>
       </v-toolbar>
 
